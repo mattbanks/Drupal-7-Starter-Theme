bf1ec1a950dca8d88909df2632b2fd2bf5473f62
Matt Banks <mjbanks@gmail.com>
2012-11-30 15:33:27 -0500

    remove environment variable

--- config.rb
+++ config.rb
@@ -10,8 +10,6 @@ javascripts_dir = "js"
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
 
-environment = :development
-
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
